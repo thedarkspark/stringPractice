5ecbf308576592340017cf0fe64612530991d5e0
Bernard Huff <bernard.huff008@gmail.com>
2017-07-26 22:44:34 -0500

    Sweep. See if you can catch the changes.
    And here is when we learn that its really important to properly tell
    your future self things. Because all things considered, he might just
    forget what we were doing with this code.

--- stringPractice.playground/Contents.swift
+++ stringPractice.playground/Contents.swift
@@ -8,7 +8,11 @@ import UIKit
 
 var learningCurve = "For the love of God!"
 
-var test = 73 
+var test = 73
+
+var explanation:String = "Okay now its time to get to the real test....s!"
+
+// Above
 
 print(learningCurve)
 
@@ -17,7 +21,13 @@ let greeting = " The first thing to say is \"Hello World\""
 print(greeting)
 
 //use contains() method to search for string within a string
-if greeting.contains("thing to say") {print ("You should say this")}
+if greeting.contains("thing to say") {
+
+    greeting.replacingOccurrences(of: "thing", with: "SHIT")
+  
+    //Yea so apparently you have tourette or something.}
+
+var rest = 8
 
 
 //to check if string is empty
@@ -39,3 +49,4 @@ if name.characters.count < 8 { print ("This name is too short!") }
 let someCharacter: Character = "e"
 switch someCharacter { case "a", "e", "i", "o", "u" : print("\(someCharacter) is a vowel.")
 default: print("\(someCharacter) is not a vowel.") }
+}
